4f79e8e0c3edbd4b4d4984585707e1779f672fd3
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2015-01-23 18:53:50 +0100

    delete of dosed ingredient

--- app/controllers/dosed_ingredients_controller.rb
+++ app/controllers/dosed_ingredients_controller.rb
@@ -58,7 +58,7 @@ class DosedIngredientsController < ApplicationController
   def destroy
     @dosed_ingredient.destroy
     respond_to do |format|
-      format.html { redirect_to dosed_ingredients_url, notice: 'Dosed ingredient was successfully destroyed.' }
+      format.html { redirect_to @dosed_ingredient.meal, notice: 'Dosed ingredient was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
