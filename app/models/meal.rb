7af8bd5074e71d742149ea730d5bbeebbf4c8a7e
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2015-01-23 22:46:52 +0100

    summary of meal values

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,24 @@
 class Meal < ActiveRecord::Base
   has_many :dosed_ingredients, dependent: :destroy
+  def calories
+    sum(:calories)
+  end
+
+  def carbs
+    sum(:carbs)
+  end
+
+  def proteins
+    sum(:proteins)
+  end
+
+  def fat
+    sum(:fat)
+  end
+
+  private
+
+  def sum(attr_name)
+    dosed_ingredients.reduce(0.0){|m, ingredient| m + ingredient.send(attr_name)}
+  end
 end
