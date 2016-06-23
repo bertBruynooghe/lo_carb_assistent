1b8774083bda17d11b08b64fc6a8f39b0934b5df
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-06-23 16:54:26 +0200

    proper support for inserting insulin doses on # devices

--- app/models/insulin_dose.rb
+++ app/models/insulin_dose.rb
@@ -1,4 +1,30 @@
 class InsulinDose < ActiveRecord::Base
   belongs_to :insulin
   default_scope { order(application_time: :desc) }
+
+  def dose_fractional
+    dose_parts.last.to_i
+  end
+
+  def dose_integral
+    dose_parts.first.to_i
+  end
+
+  def dose_fractional=(value)
+    self.dose = "#{dose_parts.first}.#{value}".to_f
+  end
+
+  def dose_integral=(value)
+    self.dose = "#{value}.#{dose_parts.last}".to_f
+  end
+
+  def save
+    byebug
+    super
+  end
+
+  private
+  def dose_parts
+    dose.to_f.to_s.split('.')
+  end
 end
