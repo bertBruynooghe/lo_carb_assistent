827cb308383c10648be5bd4df7b81a3bc36f53f3
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-06-24 09:39:48 +0200

    added split_float_fields to form_builder

--- app/form_helpers/form_builder.rb
+++ app/form_helpers/form_builder.rb
@@ -1,6 +1,15 @@
 class FormBuilder < ActionView::Helpers::FormBuilder
   def datetime_local_select(method)
+    #TODO: get rid of this js thing here
+    script = "$(function(){ timeSelectInLocalTimezone('##{@object_name}_#{method}') });"
     @template.datetime_select(@object_name, method)
-     .concat(@template.javascript_tag "$(function(){ timeSelectInLocalTimezone('##{@object_name}_#{method}') });")
+     .concat(@template.javascript_tag script)
+  end
+
+  def split_float_fields(method)
+    self.label("#{method}_integral", @object.class.human_attribute_name(method))
+      .concat(self.number_field("#{method}_integral"))
+      .concat(self.label("#{method}_fractional", I18n.t('number.format.separator')))
+      .concat(self.number_field("#{method}_fractional"))
   end
 end
