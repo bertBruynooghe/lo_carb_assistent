ba46d09956d32c2de120143bf8a84d01bab2c47e
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-06-24 10:19:07 +0200

    refactored split_float_concern

--- app/models/concerns/split_float_concern.rb
+++ app/models/concerns/split_float_concern.rb
@@ -2,26 +2,42 @@ module SplitFloatConcern
   extend ActiveSupport::Concern
 
   class_methods do
-    def split_float(method)
-      define_method("#{method}_fractional") do
-        float_parts(method).last.to_i
-      end
+    def split_float(*methods)
+      methods.each {|method| define_partial_accessors(method)}
+    end
 
-      define_method("#{method}_integral") do
-        float_parts(method).first.to_i
-      end
+    private
 
+    def define_partial_accessors(method)
+      define_method("#{method}_integral") { integral_part(method) }
+      define_method("#{method}_fractional") { fractional_part(method) }
+      define_integral_part_writer(method)
+      define_fractional_part_writer(method)
+    end
+
+    def define_fractional_part_writer(method)
       define_method("#{method}_fractional=") do |value|
-        self.send("#{method}=", "#{float_parts(method).first}.#{value}".to_f)
+        self.send("#{method}=", "#{value}.#{fractional_part(method)}".to_f)
       end
+    end
 
+    def define_integral_part_writer(method)
       define_method("#{method}_integral=") do |value|
-        self.send("#{method}=", "#{value}.#{float_parts(method).last}".to_f)
+        self.send("#{method}=", "#{integral_part(method)}.#{value}".to_f)
       end
     end
   end
 
   private
+
+  def integral_part(method)
+    float_parts(method).first.to_i
+  end
+
+  def fractional_part(method)
+    float_parts(method).last.to_i
+  end
+
   def float_parts(method)
     self.send(method).to_f.to_s.split('.')
   end
