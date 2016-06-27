1d64fdd3b301a520a2a6c222e591a5287bc0723f
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-06-27 13:19:23 +0200

    removed obsolete model code

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,7 +1,2 @@
 class Nutrient < ActiveRecord::Base
-  def assign_attributes (new_attributes)
-    %w(carbs proteins fat calories).each do |key|
-      new_attributes[key] = 0 if new_attributes[key].blank?
-    end
-  end
 end
