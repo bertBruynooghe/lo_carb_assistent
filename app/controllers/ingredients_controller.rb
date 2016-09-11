54bd7996d0b35090ae83406cbe3486755e981e06
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-09-11 20:35:24 +0200

    fixed permissions for split float ingredient quantity

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -71,10 +71,10 @@ class IngredientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ingredient_params
-      float_fields = %i(calories carbs proteins fat)
+      float_fields = %i(calories carbs proteins fat quantity)
 
       params.require(:ingredient)
         .join_split_floats(*float_fields)
-        .permit(:quantity, :name, :save_as_favorite, *float_fields)
+        .permit(:name, :save_as_favorite, *float_fields)
     end
 end
