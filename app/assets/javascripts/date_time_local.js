b8f5cb152feed5b77a2d8fff22cd921ee28326f9
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-10-10 13:40:10 +0200

    fixed some possible js issues

--- app/assets/javascripts/date_time_local.js
+++ app/assets/javascripts/date_time_local.js
@@ -2,15 +2,18 @@ $(function(){
   function inLast24Hours(date) {
     return (new Date().valueOf() - date.valueOf()) < (24 * 60 * 60 * 1000)
   }
+
   function inLastYear(date) {
     return (new Date().valueOf() - date.valueOf()) < (365 * 24 * 60 * 60 * 1000)
   }
+
   function inLastWeek(date) {
     return (new Date().valueOf() - date.valueOf()) < (7 * 24 * 60 * 60 * 1000);
   }
+  
   $('[data-transform=dateTimeLocal]').each(function(_, el){
-    date = new Date($(el).text().trim());
-    text = date.getHours() + ':' + ((100 + date.getMinutes())+'').slice(1);
+    var date = new Date($(el).text().trim());
+    var text = date.getHours() + ':' + ((100 + date.getMinutes())+'').slice(1);
     if (!inLast24Hours(date)){ text += (' ' + window.abbrDayNames[date.getDay()]); }
     if (!inLastWeek(date)) { text += (', ' + date.getDate() + '/' + (date.getMonth()+1)); }
     if (!inLastYear(date)) { text += ('/' + date.getFullYear() ); }
