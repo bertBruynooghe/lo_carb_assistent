0cb4ff4ac118a5c7c15289f6ad8c6ea23d2194a3
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-10-10 12:29:22 +0200

    clean up some javascript

--- app/assets/javascripts/date_time_local_select.js
+++ app/assets/javascripts/date_time_local_select.js
@@ -1,36 +1,36 @@
-$(document).on('ready page:load', function(){
-  function timeToSelect(id, date){
-    $(id+'_1i').val(date.getFullYear());
-    $(id+'_2i').val(date.getMonth() + 1);
-    $(id+'_3i').val(date.getDate());
-    $(id+'_4i').val((date.getHours()+100+'').slice(1));
-    $(id+'_5i').val((date.getMinutes()+100+'').slice(1));
+$(document).on('ready page:load', function() {
+  function timeToSelect(id, date) {
+    $(id + '_1i').val(date.getFullYear());
+    $(id + '_2i').val(date.getMonth() + 1);
+    $(id + '_3i').val(date.getDate());
+    $(id + '_4i').val((date.getHours() + 100 + '').slice(1));
+    $(id + '_5i').val((date.getMinutes() + 100 + '').slice(1));
   }
 
   function timeFromSelect(id) {
-    year = parseInt($(id+'_1i').val());
-    month = parseInt($(id+'_2i').val()) - 1;
-    date = parseInt($(id+'_3i').val());
-    hour = parseInt($(id+'_4i').val());
-    minute = parseInt($(id+'_5i').val());
+    var year = parseInt($(id + '_1i').val());
+    var month = parseInt($(id + '_2i').val()) - 1;
+    var date = parseInt($(id + '_3i').val());
+    var hour = parseInt($(id + '_4i').val());
+    var minute = parseInt($(id + '_5i').val());
     return new Date(year, month, date, hour, minute);
   }
 
-  function timeSelectInLocalTimezone(id){
-    offset_min = new Date().getTimezoneOffset();
-    date = timeFromSelect(id).valueOf();
+  function timeSelectInLocalTimezone(id) {
+    var offset_min = new Date().getTimezoneOffset();
+    var date = timeFromSelect(id).valueOf();
     date = new Date(date - offset_min * 60000);
     timeToSelect(id, date);
-    $(id +'_1i').parents('form').on('submit', function(){
+    $(id + '_1i').parents('form').on('submit', function() {
       date = timeFromSelect(id).valueOf();
       date = new Date(date + offset_min * 60000);
       timeToSelect(id, date);
     });
   }
 
-  $('[data-transform=dateTimeLocalSelect]').each(function(_, el){
-    id = $(el).find('select').attr('id').split('_').slice(0, -1).join('_');
-    timeSelectInLocalTimezone('#'+id);
+  $('[data-transform~=dateTimeLocalSelect]').each(function(_, el) {
+    var id = $(el).find('select').attr('id').split('_').slice(0, -1).join('_');
+    timeSelectInLocalTimezone('#' + id);
     $(el).find('.timeZone').remove();
   });
 });
