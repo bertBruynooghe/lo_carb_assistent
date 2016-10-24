a05bb861382f906f81339f2a75a6a62c89a90bca
Bert Bruynooghe <bert.bruynooghe@gmail.com>
2016-10-24 19:07:47 +0200

    support for future dates in date_time_local.js

--- app/assets/javascripts/date_time_local.js
+++ app/assets/javascripts/date_time_local.js
@@ -1,14 +1,17 @@
 $(function(){
   function inLast24Hours(date) {
-    return (new Date().valueOf() - date.valueOf()) < (24 * 60 * 60 * 1000)
+    var value = new Date().valueOf() - date.valueOf();
+    return 0 < value && value < (24 * 60 * 60 * 1000);
   }
 
   function inLastYear(date) {
-    return (new Date().valueOf() - date.valueOf()) < (365 * 24 * 60 * 60 * 1000)
+    var value = new Date().valueOf() - date.valueOf();
+    return 0 < value && value < (365 * 24 * 60 * 60 * 1000);
   }
 
   function inLastWeek(date) {
-    return (new Date().valueOf() - date.valueOf()) < (7 * 24 * 60 * 60 * 1000);
+    var value = new Date().valueOf() - date.valueOf();
+    return 0 < value && value < (7 * 24 * 60 * 60 * 1000);
   }
   
   $('[data-transform=dateTimeLocal]').each(function(_, el){
