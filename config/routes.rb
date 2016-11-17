361a0f9bba8c69c4b6bfebc44826e0ddace66137
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-11-17 17:30:54 +0100

    root redirecting to meals to prevent another file to be cached

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :insulins
   resources :blood_sugar_readings
   devise_for :users
-  root to: 'meals#index'
+  root to: redirect('meals')
 
   resources :meals, except: %i(new edit), shallow: true do
     resources :ingredients, except: %i(index new edit)
