75a59b42dbbb3482ab8bc2c461ef5a5d256a0774
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-12-17 11:31:58 +0100

    refactored graphs

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,14 +1,23 @@
 class Graph
-  def self.for_week(start_date)
-    start_date = if start_date
-      DateTime.iso8601(start_date) + 7.days
-    else
-      DateTime.now
-    end  
-    (0..6).map do |i|
-      relation = BloodSugarReading.where('read_time > ?', start_date - (i+1).days)
-                       .where('read_time <= ?', start_date - i.days)
-      Hash[relation.map{ |bsr| [bsr.read_time, bsr.value] }]
-    end
+  attr_reader :data
+  attr_reader :options
+
+  def self.for_week(start_datetime)
+    end_datetime = start_datetime ? (DateTime.iso8601(start_datetime) + 7.days) : DateTime.now
+    (0..6).map{ |i| self.graph_for_day(end_datetime - i.days) }
   end
+
+  def initialize(data)
+    @data = data
+    @options = { max: 500, curve: false }
+  end
+
+  private
+
+  def self.graph_for_day(end_datetime)
+    relation = BloodSugarReading.where('read_time > ?', end_datetime - 1.days)
+                                .where('read_time <= ?', end_datetime)
+    Graph.new(Hash[relation.map{ |bsr| [bsr.read_time, bsr.value] }])
+  end
+
 end
