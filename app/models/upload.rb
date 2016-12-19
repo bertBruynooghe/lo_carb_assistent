2132eb1d3ca35836903e1b93a795ff719b0ac19f
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-12-19 17:55:03 +0100

    extract insulin doses from upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -27,6 +27,29 @@ class Upload
   def save_line(line)
     tokens = line.split("\t")
     time = DateTime.strptime("#{tokens.second} CET", '%Y-%m-%d %H:%M %Z')
+    extract_glucose(tokens, time)
+    extract_insulin(tokens, time)
+  end
+
+  def extract_insulin(tokens, time)
+    fast = tokens[6]
+    if fast.present?
+      fast = fast.gsub(',', '.').to_f
+      if InsulinDose.where(application_time: time, dose: fast, insulin_id: 1).empty?
+        InsulinDose.create(application_time: time, dose: fast, insulin_id: 1)
+      end
+    end
+    byebug
+    slow = tokens[10]
+    if slow.present?
+      slow = slow.gsub(',', '.').to_f
+      if InsulinDose.where(application_time: time, dose: slow, insulin_id: 2).empty?
+        InsulinDose.create(application_time: time, dose: slow, insulin_id: 2)
+      end
+    end
+  end
+
+  def extract_glucose(tokens, time)
     glucose = tokens.fourth
     glucose = tokens.fifth unless glucose.present?
     if glucose.present?
