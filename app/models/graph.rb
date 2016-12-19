87c1f259de1b4ad08677ab0e1b7781d9ba0794b1
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-12-19 09:12:45 +0100

    refactor

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -15,13 +15,10 @@ class Graph
   private
 
   def self.graph_for_day(end_datetime)
-    glucose = BloodSugarReading.where('read_time > ?', end_datetime - 1.days)
-                                .where('read_time <= ?', end_datetime)
-
-    glucose = Hash[glucose.map{ |bsr| [bsr.read_time, bsr.value] }]
 
 
-    Graph.new([ colorSetting(glucose.map{ |k, v| to_color(v) }).merge(name: 'glucose', data: glucose),
+
+    Graph.new([ blood_sugar_readings_data(end_datetime),
                 carbs_data(end_datetime),
                 basal_data(end_datetime),
                 indicator_line(end_datetime, 65),
@@ -29,19 +26,7 @@ class Graph
               ])
   end
 
-  def self.colorSetting(colors)
-    colors.unshift('black')
-    {
-      library:
-      {
-        borderColor: colors,
-        pointBorderColor: colors,
-        pointBackgroundColor: colors,
-        backgroundColor: colors,
-        spanGaps: true
-      }
-    }
-  end
+
 
   def self.indicator_line(end_datetime, value)
     {
@@ -50,10 +35,39 @@ class Graph
     }
   end
 
+  def self.blood_sugar_readings_data(end_datetime)
+    blood_sugar_readings = BloodSugarReading.where('read_time > ?', end_datetime - 1.days)
+                                .where('read_time <= ?', end_datetime)
+
+    # TODO: only put color when new time is written in hash...
+    data = { name: 'glucose', data: {}, library: { spanGaps: true, borderColor: ['black'],
+    pointBorderColor: ['black'],
+    pointBackgroundColor: ['black'],
+    backgroundColor: ['black']}}
+    blood_sugar_readings.each do |reading|
+      data[:data][reading.read_time] = reading.value
+      lib = data[:library]
+      lib[:borderColor] << to_color(reading.value)
+      lib[:pointBorderColor] << to_color(reading.value)
+      lib[:pointBackgroundColor] << to_color(reading.value)
+      lib[:backgroundColor] << to_color(reading.value)
+    end
+    data
+  end
+
   def self.carbs_data(end_datetime)
     meals = Meal.where('consumption_time > ?', end_datetime - 1.days)
                 .where('consumption_time <= ?', end_datetime)
-    data = { name: 'gKH', data: {}, library: { showLine: false, pointStyle: 'triangle', pointHitRadius: 0, pointHoverRadius: [100], pointRadius: [100], pointBackgroundColor: "rgba(255, 0, 0, 0.2)", pointBorderColor: "rgba(255, 0, 0, 0.2)" } }
+    data = { name: 'gKH', data: {},
+             library: {
+               showLine: false,
+               pointStyle: 'triangle',
+               pointHitRadius: 0,
+               pointHoverRadius: [100],
+               pointRadius: [100],
+               pointBackgroundColor: "rgba(255, 0, 0, 0.2)",
+               pointBorderColor: "rgba(255, 0, 0, 0.2)" } }
+
     meals.each do |meal|
       data[:data][meal.consumption_time] = meal.carbs
       data[:library][:pointRadius] << meal.carbs
