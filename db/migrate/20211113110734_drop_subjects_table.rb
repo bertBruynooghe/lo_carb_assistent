53eefc145ee0440a5d6be9765598f0be99ad8611
Bert Simon Bruynooghe <bert@bruynooghe-polet.com>
2021-11-13 17:57:47 +0100

    removed migrations

--- db/migrate/20211113110734_drop_subjects_table.rb
+++
@@ -1,9 +0,0 @@
-class DropSubjectsTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :subjects do |t|
-      t.string "name"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-  end
-end
