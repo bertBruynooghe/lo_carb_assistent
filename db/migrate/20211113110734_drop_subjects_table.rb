17dd584724ee7a1dca21b23ea58557053e083828
Bert Simon Bruynooghe <bert@bruynooghe-polet.com>
2021-11-13 13:06:27 +0100

    fix migration

--- db/migrate/20211113110734_drop_subjects_table.rb
+++ db/migrate/20211113110734_drop_subjects_table.rb
@@ -1,8 +1,5 @@
 class DropSubjectsTable < ActiveRecord::Migration[6.1]
   def change
-    remove_reference :meals, :subject, index: true, foreign_key: true
-    remove_reference :nutrients, :subject, index: true, foreign_key: true
-    remove_reference :users, :subject, index: true, foreign_key: true
     drop_table :subjects do |t|
       t.string "name"
       t.datetime "created_at", null: false
