582eb80f996bc30a0d156a9e582a864a7e933637
Bert Simon Bruynooghe <bert@bruynooghe-polet.com>
2021-11-18 14:34:08 +0100

    added icons

--- app/controllers/webmanifest_controller.rb
+++ app/controllers/webmanifest_controller.rb
@@ -10,7 +10,49 @@ class WebmanifestController < ApplicationController
         theme_color: "#000000",
         background_color: "#FFFFFF",
         display: "fullscreen",
-        orientation: "portrait"
+        orientation: "portrait",
+        icons: [
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x48.png'),
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x72.png'),
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x96.png'),
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x128.png'),
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x192.png'),
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x384.png'),
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon_x512.png'),
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          { 
+            src: ActionController::Base.helpers.image_path('maskable_icon.png'),
+            sizes: '610x610',
+            type: 'image/png'
+          }
+        ]
       }
     }
     end
