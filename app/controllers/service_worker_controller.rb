cfa84fc28a00ed21e99c41dbc2acc61a229152e1
Bert Simon Bruynooghe <bert@bruynooghe-polet.com>
2021-11-19 20:34:12 +0100

    fixed sourcemap of service_worker

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -7,10 +7,19 @@ class ServiceWorkerController < ApplicationController
     def index
         # next solution is rather hacky, it resolves the erb of the pack AFTER building them, 
         # which poses a bit af a risk if somewhere in the files a `<%` appears
+        # TODO: replace //# sourceMappingURL= with right path for sourcemapping to work
         respond_to do |format|
           format.js {
-            render inline: File.read(Rails.root.join("public", ActionController::Base.helpers.asset_pack_path("service_worker.js")[1..])), cache: true
+            render inline: getJs(), cache: true
           }
         end
+    end
+
+    def getJs
+      service_worker_js_path = ActionController::Base.helpers.asset_pack_path('service_worker.js')
+      file_path = Rails.root.join('public', service_worker_js_path[1..])
+      content = File.read(file_path)
+      pack_folder_url = service_worker_js_path.split('/')[...-1].join('/')
+      content.gsub(/^\/\/# sourceMappingURL=/, '//# sourceMappingURL=' + pack_folder_url + '/')
     end
 end
