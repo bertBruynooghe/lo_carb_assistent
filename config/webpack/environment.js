42d2be4482177648280c436c232c548c87a49878
Bert Simon Bruynooghe <bert@bruynooghe-polet.com>
2021-12-10 14:33:02 +0100

    spike: loading html.erb files in webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,33 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
-const htmlLoader = {
-    test: /\.html(\.erb)?$/i,
-    loader: 'html-loader',
+// TODO: this doesn't work to render htmls from views, 
+// since the views contain references to webpack compiled files,
+// which are not available at webpack build time.
+
+const htmlErbLoader = {
+  test: /\.html\.erb$/,
+  enforce: 'pre',
+  exclude: /node_modules/,
+  use: [
+    { 
+      loader: 'file-loader',
+      options: {
+        esModule:false,
+        context: 'app/javascript'
+      }
+    },
+    {
+      loader: 'rails-erb-loader',
+      options: {
+        runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+      }
+    }
+
+  ]
 }
 
-environment.loaders.append('html', htmlLoader)
 environment.loaders.prepend('erb', erb)
+environment.loaders.prepend('htmlErb', htmlErbLoader )
+
 module.exports = environment
